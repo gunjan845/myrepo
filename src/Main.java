fa58d2509f2bfe2c62225fb3e9827da7e233a48b
gunjan845 <gunjanmeena81@gmail.com>
2025-12-29 18:01:15 +0530

    hello

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 <<<<<<< HEAD
-        System.out.printf("hello master...");
+        System.out.printf("hello");
 =======
         System.out.printf("hello  from gunjan-dev....");
 >>>>>>> gunjan-dev
